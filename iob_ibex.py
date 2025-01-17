ac22cc61fde1aa016fb1ad7361f0127992398328
OJaimeDelas <minde.jaime@gmail.com>
2025-01-17 13:26:27 +0000

    interupted generation of .v

--- iob_ibex.py
+++ iob_ibex.py
@@ -6,7 +6,7 @@
 def setup(py_params_dict):
     attributes_dict = {
         "version": "0.1",
-        "version": "0.1",
+        "generate_hw": False,
         "confs": [
             {
                 "name": "AXI_ID_W",
